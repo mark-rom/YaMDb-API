46b178faf334415002170b38e218504dc2123322
Artem <fabrikoff.artyom@yandex.ru>
2022-04-12 00:07:59 +0300

    patch utf-8  management_commands populate_db

--- api_yamdb/reviews/management/commands/populate_users.py
+++ api_yamdb/reviews/management/commands/populate_users.py
@@ -54,7 +54,7 @@ class Command(BaseCommand):
         file_path = self.get_csv_file(filename)
         line_count = 0
         try:
-            with open(file_path) as csv_file:
+            with open(file_path, encoding='utf-8') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
                 self.clear_model()
                 for row in csv_reader:
