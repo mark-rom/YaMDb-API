78e1a09dc15b948fb0a8d3f2ad74144c2ea3fe3c
Artem <fabrikoff.artyom@yandex.ru>
2022-04-11 23:24:04 +0300

    Для git pull делаю коммит что бы зафиксировать локальные изменения, комит не несет полезную нагрузку

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,9 +3,9 @@ from reviews import models
 
 
 class TitleFilter(django_filters.FilterSet):
-    category = django_filters.CharFilter(field_name='category__slug'),
-    genre = django_filters.CharFilter(field_name='genre__slug'),
-    name = django_filters.CharFilter(field_name='name'),
+    category = django_filters.CharFilter(field_name='category__slug')
+    genre = django_filters.CharFilter(field_name='genre__slug')
+    name = django_filters.CharFilter(field_name='name')
     year = django_filters.NumberFilter(field_name='year')
 
     class Meta:
